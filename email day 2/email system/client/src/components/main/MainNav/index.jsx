9e94dfc19e2e6a73707329a19910b2a85f0058ac
Ahron Cohen <aharoncohen7@gmail.com>
2024-04-04 04:02:28 +0300

    new icons

--- email day 2/email system/client/src/components/main/MainNav/index.jsx
+++ email day 2/email system/client/src/components/main/MainNav/index.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
-import { GoEye } from "react-icons/go";
+
 import { IoMdContacts } from "react-icons/io";
-import { VscGraph } from "react-icons/vsc";
+import { IoMdStats } from "react-icons/io";
 import { IoVideocam } from "react-icons/io5";
 import { MdAvTimer } from "react-icons/md";
 import { BsClipboardCheck } from "react-icons/bs";
+import { FaRegEye } from "react-icons/fa";
 import { NavLink } from 'react-router-dom';
 import styles from './style.module.css'
 
 const mainNavIcons = [
     { icon: MdAvTimer, name: "Timer" },
     { icon: BsClipboardCheck, name: "Tasks" },
-    { icon: GoEye, name: "Watch" },
+    { icon: FaRegEye, name: "Watch" },
     { icon: IoMdContacts, name: "Emails" },
-    { icon: VscGraph, name: "Stats" },
+    { icon: IoMdStats, name: "Stats" },
     { icon: IoVideocam, name: "Video" }
 ]
 
