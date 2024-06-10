3a502147bed5e97683e1f03bf65abefef46cb421
Ahron Cohen <aharoncohen7@gmail.com>
2024-06-10 03:53:02 +0300

    print pdf

---
+++ email day 2/email system/server/routes/img.router.js
@@ -0,0 +1,24 @@
+const express = require("express")//ייבוא ספריה שמסוגלת לייצר 
+const imgRouter = express.Router()
+const cloudinary = require("../cloudnary");
+
+// יצירת URLs
+imgRouter.post("/get-url", async (req, res) => {
+    console.log("start get-url");
+
+    try {
+        const { body } = req;
+        const result = await cloudinary.uploader.upload(body.img,{
+            folder: "chat-images"
+        })
+        console.log("🚀 ~ authRouter.post ~ body:", result.secure_url)
+        res.send(result.secure_url)
+    }
+    catch (err) {
+        res.status(500).send(err.msg || err.message || "wrong")
+    }
+});
+
+
+
+module.exports = { imgRouter };
